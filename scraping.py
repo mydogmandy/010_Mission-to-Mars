97280efc7206859519bbc6469fbf1147d62e876d
mydogmandy <62800249+mydogmandy@users.noreply.github.com>
2020-06-04 12:17:59 -0500

    updated scraping.py

--- scraping.py
+++ scraping.py
@@ -27,31 +27,34 @@ executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
 browser = Browser('chrome', **executable_path)
 
 def mars_news(browser):
+    try: 
+        # Visit the Mars NASA news site:
+        url = 'https://mars.nasa.gov/news'
+        browser.visit(url)
 
-    # Visit the Mars NASA news site:
-    url = 'https://mars.nasa.gov/news'
-    browser.visit(url)
+        # Optional delay for loading the page
+        browser.is_element_present_by_css('ul.item_list li.slide', wait_time=1)
 
-    # Optional delay for loading the page
-    browser.is_element_present_by_css('ul.item_list li.slide', wait_time=1)
+        # Convert the browser html to a soup object and then quit the browser:
+        html = browser.html
+        news_soup = BeautifulSoup(html, 'html.parser')
 
-    # Convert the browser html to a soup object and then quit the browser:
-    html = browser.html
-    news_soup = BeautifulSoup(html, 'html.parser')
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
+        slide_elem.find('div', class_='content_title')
 
-    slide_elem = news_soup.select_one('ul.item_list li.slide')
-    slide_elem.find('div', class_='content_title')
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide_elem.find('div', class_='content_title').get_text()
 
-    # Use the parent element to find the first 'a' tag and save it as 'news_title'
-    news_title = slide_elem.find('div', class_='content_title').get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 
-    # Use the parent element to find the paragraph text
-    news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-
-    return news_title, news_p
+        return news_title, news_p
+    except AttributeError:
+        print("found attribute error")
+        return None, None
 
 # Add try/except for error handling:
-try:
+#try:
     slide_elem = news_soup.select_one("ul.item_list li.slide")
 
         # Use the parent element to find the first 'a' tag and save it as 'news_title'
@@ -59,37 +62,33 @@ try:
 
         # Use the parent element to find the papagraph text
     news_p = slide_elem.find("div", class_="article_teaser_body").get_text()
-except AttributeError:
-    return None, None
+#except AttributeError:
+#    return None, None
 
 
 # Featured Images
 
 def featured_image(browser):    
-
+    try:
+    # find the relative image url:
+        img_url_rel = img_soup.select_one('figure.lede a img').get("src")
     # Visit URL
-    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&catagory=Mars'
-    browser.visit(url)
+        url = 'https://www.jpl.nasa.gov/spaceimages/?search=&catagory=Mars'
+        browser.visit(url)
 
     # Find and click the full image button
-    full_image_elem = browser.find_by_id('full_image')
-    full_image_elem.click()
+        full_image_elem = browser.find_by_id('full_image')
+        full_image_elem.click()
 
     # Find the more info button and click that:
-    browser.is_element_present_by_text('more info', wait_time=1)
-    more_info_elem = browser.links.find_by_partial_text('more info')
-    more_info_elem.click()
+        browser.is_element_present_by_text('more info', wait_time=1)
+        more_info_elem = browser.links.find_by_partial_text('more info')
+        more_info_elem.click()
 
-    return img_url
-
+        return img_url
+    except AttributeError:
+        return None
 # Add try/except for error handling:
-
-try:
-    # find the relative image url:
-    img_url_rel = img_soup.select_one('figure.lede a img').get("src")
-
-except AttributeError:
-    return None
 
 def mars_facts():
 
