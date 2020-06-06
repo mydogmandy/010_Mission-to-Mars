5582458b7e09a2fc7b6af71ac33765fa974136ce
mydogmandy <62800249+mydogmandy@users.noreply.github.com>
2020-06-06 14:06:45 -0500

    updates

--- scraping2.py
+++ scraping2.py
@@ -11,12 +11,14 @@ def scrape_all():
 
     # Run all scraping functions and store results in dictionary
     data = {
-        "news_title": news_title,
-        "news_paragraph": news_paragraph,
-        "featured_image": featured_image(browser),
-        "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        #"news_title": news_title,
+        #"news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser)
+        #"facts": mars_facts(),
+        #"last_modified": dt.datetime.now()
         }
+    
+    # Quit the browser & retrieve the data gathered:
     browser.quit()    
     return data
     
@@ -39,35 +41,40 @@ def mars_news(browser):
         # Use the parent element to find the paragraph text
         news_p = slide_elem.find("div", class_="article_teaser_body").get_text()    
 
-        
-
         return news_title, news_p
     except AttributeError:
         return None, None
 
 def featured_image(browser):
+    # Visit the NASA Mars site:
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&catagory=Mars'
+    browser.visit(url)
+
+    # Click on the button to get the full size image:
+    full_image_elem = browser.find_by_id('full_image')
+    full_image_elem.click()
+    
+    # Click on the 'More Info' button:
+    browser.is_element_present_by_text('more info', wait_time=1)
+    more_info_elem = browser.links.find_by_partial_text('more info')
+    more_info_elem.click()
+    
+    # Parse the results:
     html = browser.html
     img_soup = BeautifulSoup(html, 'html.parser')
-    try:
-        url = 'https://www.jpl.nasa.gov/spaceimages/?search=&catagory=Mars'
-        browser.visit(url)
-        # find the relative image url
-        img_url_rel = img_soup.select_one('figure.lede a img').get("src")
-
-        full_image_elem = browser.find_by_id('full_image')
-        full_image_elem.click()
-
-        browser.is_element_present_by_text('more info', wait_time=1)
-        more_info_elem = browser.links.find_by_partial_text('more info')
-        more_info_elem.click()
+     
+    # find the relative image url
+    img_url_rel = img_soup.select_one('figure.lede a img')
     
-        img_url_rel = img_soup.select_one('figure.lede a img').get('src')
-
-        img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
-
-        return img_url
-    except AttributeError: 
+    # Use try-except to keep program running if an image retrieval error:
+    try:
+        img_url = img_url_rel.get("src")
+    except AttributeError:     
         return None
+
+    # Return the full-size image:    
+    img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+    return img_url  
 
 def mars_facts():
 
